3c4ee961e9c77cd2560cca34b7c03dc28c67dc02
AnjalyKiran <anjalyhere@gmail.com>
2026-04-29 20:41:36 +0200

    chore: update environment configuration"

--- fixtures/baseTest.ts
+++ fixtures/baseTest.ts
@@ -3,27 +3,48 @@ import { HomeActions } from "../pages/home/home.actions";
 import { ProductActions } from "../pages/product/product.actions";
 import { CartActions } from "../pages/cart/cart.actions";
 import { LoginActions } from "../pages/login/login.actions";
+import { RegisterActions } from "../pages/register/register.actions";
+import { CheckoutActions } from "../pages/checkout/checkout.actions";
+import { SuccessActions } from "../pages/success/success.actions";
 
-type Fixtures = {
+type Pages = {
   home: HomeActions;
   product: ProductActions;
   cart: CartActions;
   login: LoginActions;
+  register: RegisterActions;
+  checkout: CheckoutActions;
+  success: SuccessActions;
 };
 
-export const test = base.extend<Fixtures>({
+export const test = base.extend<Pages>({
   home: async ({ page }, use) => {
     await use(new HomeActions(page));
   },
+
   product: async ({ page }, use) => {
     await use(new ProductActions(page));
   },
+
   cart: async ({ page }, use) => {
     await use(new CartActions(page));
   },
+
   login: async ({ page }, use) => {
     await use(new LoginActions(page));
-  }
+  },
+
+  register: async ({ page }, use) => {
+    await use(new RegisterActions(page));
+  },
+
+  checkout: async ({ page }, use) => {
+    await use(new CheckoutActions(page));
+  },
+
+  success: async ({ page }, use) => {
+    await use(new SuccessActions(page));
+  },
 });
 
-export const expect = test.expect;
+export { expect } from "@playwright/test";
